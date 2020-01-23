f8df348817cae7e33d549f631a55a3eebfbc1dcc
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2020-01-23 22:56:25 +0330

    Add project files.

---
+++ MyApplication/MainForm.cs
@@ -0,0 +1,17 @@
+﻿//using System.Linq;
+
+namespace MyApplication
+{
+	public partial class MainForm : System.Windows.Forms.Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void MainForm_Load(object sender, System.EventArgs e)
+		{
+
+		}
+	}
+}
